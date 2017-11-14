50e10e96015716764cf4204969ab818ddd8f8720
max <maxc@maxc.in>
2017-11-14 20:51:37 +0000

    fixed iptables

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ cp -f port-tester-ping-udp.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable port-tester-ping-udp.service
 systemctl start port-tester-ping-udp.service
-iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j REDIRECT --to-ports 7777
+iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j REDIRECT --to-ports 7777
